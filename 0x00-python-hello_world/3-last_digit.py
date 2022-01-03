a77bbf998bfde4467a752905ba3c320557e57a73
Tim <tsimms@gmail.com>
2022-01-03 16:23:31 -0600

    pep8

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -6,10 +6,10 @@ if number < 0:
 else:
     last_digit = number % 10
 
-
+print("Last digit of {} is".format(number), end =" ")
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    print("{} and is greater than 5".format(last_digit))
 elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("{} and is 0".format(last_digit))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print("{} and is less than 6 and not 0".format(last_digit))
