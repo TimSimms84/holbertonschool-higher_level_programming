16125ce8245ad5c522a0ebc29f9865314c0a6c32
Tim <tsimms@gmail.com>
2022-01-05 12:56:00 -0600

    repush of 1

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
-import sys
 
-j = len(sys.argv)
+def argvprint(argv):
+    j = len(argv)
 
+    if j == 1:
+        print("0 arguments.")
+    elif j == 2:
+        print("1 argument\n1: {}".format(str(sys.argv[1])))
+    else:
+        print("{} arguments:".format(j - 1))
+        for i in range(len(sys.argv)):
+            if i == 0:
+                continue
+            print("{}: {}".format(i, str(sys.argv[i])))
 
-if j == 1:
-    print("0 arguments.")
-elif j == 2:
-    print("1 argument\n1: {}".format(str(sys.argv[1])))
-else:
-    print("{} arguments:".format(j - 1))
-    for i in range(len(sys.argv)):
-        if i == 0:
-            continue
-        print("{}: {}".format(i, str(sys.argv[i])))
+if __name__ == '__main__':
+    import sys
+    argvprint(sys.argv)
