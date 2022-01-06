66fa568d32840e4a564c5f0126dd3f9f897806b6
Tim <tsimms@gmail.com>
2022-01-06 15:12:49 -0600

    edge casing

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 
 def multiple_returns(sentence):
-    if sentence:
-        return len(sentence), sentence[0]
-    else:
+    if len(sentence) == 0:
         return 4, 'N'
+    else:
+        return len(sentence), sentence[0]
