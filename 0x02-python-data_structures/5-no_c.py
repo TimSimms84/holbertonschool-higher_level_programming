49b5e5feb4035d7ff18f37a70b3a90ca3a943f24
Tim <tsimms@gmail.com>
2022-01-06 14:36:07 -0600

    don't use replace!

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -2,11 +2,5 @@
 
 
 def no_c(my_list):
-    no_c = "C,c"
-    new_string = my_list[:]
-
-    for character in no_c:
-        new_string = new_string.replace(character, "")
-        return new_string
-    else:
-        return new_string
+    new_list = my_list.translate({ord(i): None for i in 'cC'})
+    return new_list
