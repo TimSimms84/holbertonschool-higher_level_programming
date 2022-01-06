3ae507cbd97454d7713b7cbb3400d05f9610cda4
Tim <tsimms@gmail.com>
2022-01-06 16:54:54 -0600

    made it better

--- 0x02-python-data_structures/105-big_diff.py
+++ 0x02-python-data_structures/105-big_diff.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 def big_diff(my_list=[]):
     if len(my_list) < 2:
         return 0
-    if len(my_list) > 1:
+    else:
         max, min = my_list[0], my_list[0]
         for i in my_list:
             if i > max:
                 max = i
-        for i in my_list:
             if i < min:
                 min = i
         return max - min
