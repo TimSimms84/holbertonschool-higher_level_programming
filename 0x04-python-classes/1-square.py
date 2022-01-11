4709bf2880f6991b1fbb0260c51e8b0ebd12adec
Tim <tsimms@gmail.com>
2022-01-11 13:20:06 -0600

    read the requirements better

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -4,5 +4,9 @@
 
 class Square:
     "square class with only a private size"
-    def __init__(self, size):
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
