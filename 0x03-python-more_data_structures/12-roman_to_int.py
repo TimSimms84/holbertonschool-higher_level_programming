9df27ebe4baa0ca1a60dcaeb6f4a389063e8dc8a
Tim <tsimms@gmail.com>
2022-01-10 19:32:37 -0600

    romans

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    dec, ans = 0
+    dec, ans = 0, 0
     for i in range(len(roman_string) - 1, -1, -1):
         if roman[roman_string[i]] >= dec:
             ans += roman[roman_string[i]]
