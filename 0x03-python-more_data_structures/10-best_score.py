949496260e4f9d4e01ea30ce576742d96875db6a
Tim <tsimms@gmail.com>
2022-01-10 18:35:46 -0600

    want the key, not the value

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        a = sorted(a_dictionary.items(), key=lambda x: x[1])
-        return a[-1]
-    else:
-        return None
+        best = 0
+        for key, value in a_dictionary.items():
+            if value > best:
+                best, newkey = value, key
+        return newkey
+
