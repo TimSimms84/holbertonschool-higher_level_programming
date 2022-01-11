72664b74859e7fc21cf987e54540927b22da45ef
Tim <tsimms@gmail.com>
2022-01-10 19:58:06 -0600

    weighted average

--- 0x03-python-more_data_structures/100-weight_average.py
+++ 0x03-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,6 @@
 def weight_average(my_list=[]):
     if my_list:
         s = sum([x * y for x, y in my_list])
-        res = s / sum([y for x, y in my_list])
-        return res
+        average = s / sum([y for x, y in my_list])
+        return average
     return 0
