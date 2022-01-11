300d640efafa8cc642bd308f15148163e0920525
Tim <tsimms@gmail.com>
2022-01-10 19:41:34 -0600

    return 0

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    dec, ans = 0, 0
-    for i in range(len(roman_string) - 1, -1, -1):
-        if roman[roman_string[i]] >= dec:
-            ans += roman[roman_string[i]]
-        else:
-            ans -= roman[roman_string[i]]
-        dec = roman[roman_string[i]]
-    return(ans)
+    if roman_string is not None and type(roman_string) is  str:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        dec, ans = 0, 0
+        for i in range(len(roman_string) - 1, -1, -1):
+            if roman[roman_string[i]] >= dec:
+                ans += roman[roman_string[i]]
+            else:
+                ans -= roman[roman_string[i]]
+            dec = roman[roman_string[i]]
+        return ans
+    return 0
