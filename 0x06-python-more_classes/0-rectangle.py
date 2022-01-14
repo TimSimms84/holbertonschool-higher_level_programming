8a547c72c487337f3fee816310355a07dc12bae0
Tim <tsimms@gmail.com>
2022-01-14 14:29:42 -0600

    why isn't this type check working?

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -21,15 +21,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """
-        width setter
-        """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """
