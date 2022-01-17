e65a3ece863dd7a6c2ed15216b8772c0819255cf
Tim <tsimms@gmail.com>
2022-01-17 12:39:44 -0600

    revert

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,7 @@ matrix_divided:
     two dec places
 """
 
-
+list_error = "matrix must be a matrix (list of lists) of integers/floats"
 def matrix_divided(matrix, div):
     """
     Checks if matrix is a list
@@ -15,16 +15,14 @@ def matrix_divided(matrix, div):
     Checks div is not 0
     """
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
+        raise TypeError(list_error)
     size = len(matrix[0])
     for row in matrix:
         if size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         for i in row:
             if type(i) not in (int, float):
-                raise TypeError("matrix must be a matrix (list of lists) of \
-                    integers/floats")
+                raise TypeError(list_error)
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
     if div == 0:
