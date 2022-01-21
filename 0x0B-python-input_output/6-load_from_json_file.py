27c662179fbf1425079bb5bdfb310dba8fc080e6
Tim <tsimms@gmail.com>
2022-01-21 14:11:18 -0600

    add 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+ writes an Object to a text file, using a JSON representation:
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+     writes an Object to a text file, using a JSON representation:
+    """
+    with open(filename, "r", encoding="utf-8") as my_file:
+        return json.load(my_file)
