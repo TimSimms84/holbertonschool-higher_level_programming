b6682c97889e39811689ea6e0540369847038cea
Tim <tsimms@gmail.com>
2022-01-21 17:07:32 -0600

    stupid magic 11 don't work right

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,10 +9,11 @@ def pascal_triangle(n):
     Returns: list of pascal's Triangle
     """
     triangle = []
-    if n <= 0:
-        return triangle
     for i in range(n):
-        angle = 11 ** i
-        tri = [int(j) for j in str(angle)]
-        triangle.append(tri)
+        triangle.append([])
+        for j in range(i + 1):
+            if j == i or j == 0:
+                triangle.append(1)
+            else:
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
     return triangle
