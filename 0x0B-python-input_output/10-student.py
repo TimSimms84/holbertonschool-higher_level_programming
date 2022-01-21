a610386bd4c7482e743e97adb437a45215650467
Tim <tsimms@gmail.com>
2022-01-21 15:22:15 -0600

    add 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Student class that defines a student by public: firstname, listname, age
+"""
+
+
+from attr import attrs
+
+
+class Student():
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary representation of a Student instance """
+        if attrs:
+            new_dict = {}
+            for i in attrs:
+                try:
+                    new_dict[i] = vars(self)[i]
+                except Exception:
+                    pass
+            return new_dict
+        return vars(self)
