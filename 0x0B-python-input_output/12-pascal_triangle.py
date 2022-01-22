c36b326a6f5ea3b5ee6eead40efa63d317d5d943
Tim <tsimms@gmail.com>
2022-01-22 09:09:55 -0600

    fix 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,7 +13,7 @@ def pascal_triangle(n):
         triangle.append([])
         for j in range(i + 1):
             if j == i or j == 0:
-                triangle.append(1)
+                triangle[i].append(1)
             else:
                 triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
     return triangle
