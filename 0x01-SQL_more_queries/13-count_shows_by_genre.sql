a023bc8b355dbb577a33fb7397b6e55bf3ba4665
Tim <tsimms@gmail.com>
2022-02-10 21:14:25 -0600

    more answers

---
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+--
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY COUNT(tv_show_genres.show_id) DESC;
---
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+ -- lists all shows, and all genres linked to that show, from the database
+SELECT shows.title, genre.name
+FROM tv_shows AS shows
+LEFT JOIN tv_show_genres AS tvshowgenre
+ON shows.id = tvshowgenre.show_id
+LEFT JOIN tv_genres AS genre
+ON tvshowgenre.genre_id = genre.id
+ORDER BY shows.title ASC, genre.name ASC;
