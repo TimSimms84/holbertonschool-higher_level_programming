040f07aa9649bb311b84f5cf964e1c1590970cac
Tim <tsimms@gmail.com>
2022-02-10 21:20:21 -0600

    fix column name

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
---
+-- list all genres from hbtn_0d_tvshows
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_show_genres
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY COUNT(tv_show_genres.show_id) DESC;
