5a42aa8dfa14fee15a7e7420348871e8a85e5a9a
Tim <tsimms@gmail.com>
2022-02-14 10:13:57 -0600

    change REFERENCES

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -4,4 +4,4 @@ USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
        state_id INT NOT NULL,
 	   name VARCHAR(256) NOT NULL,
-	   FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+	   FOREIGN KEY(state_id) REFERENCES states(id));
