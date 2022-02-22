f8d9b121c0ea328f650f01f758b782d57056229f
Tim <tsimms@gmail.com>
2022-02-22 15:25:09 -0600

    push 1

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+
+def print_n_state():
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states")
+    for rows in cursor.fetchall():
+        if rows[1][0] == 'N':
+            print(rows)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_n_state()
