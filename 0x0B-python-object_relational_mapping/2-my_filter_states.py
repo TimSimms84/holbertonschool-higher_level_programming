4921886918f631af7968ede56f41b89947ac49ba
Tim <tsimms@gmail.com>
2022-02-25 09:40:02 -0600

    try binary

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,13 @@ import MySQLdb
 
 def print_n_state():
     from sys import argv
-    db = MySQLdb.connect(host="localhost", user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", [format(argv[4])])
+    cur.execute("SELECT * FROM states WHERE name\
+        LIKE binary %s", [format(argv[4])])
     for rows in cur.fetchall():
         print(rows)
 
