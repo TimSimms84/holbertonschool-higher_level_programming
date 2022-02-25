6e93355c8a83d79e849bc212ed311a93a3760c48
Tim <tsimms@gmail.com>
2022-02-24 20:34:23 -0600

    test

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 
 def print_n_state():
     from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], database=argv[3])
 
     cur = db.cursor()
