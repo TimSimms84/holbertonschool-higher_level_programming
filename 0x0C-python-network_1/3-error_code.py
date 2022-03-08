9df89e35e8ddb43c4c372e21164463ef27e713b1
Tim <tsimms@gmail.com>
2022-03-08 13:29:40 -0600

    push 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
+
+
+def hbtn_header():
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    from sys import argv
+    """gets status of hbtn intranet"""
+    email = {'email': argv[2]}
+    data = urlencode(email).encode("utf-8")
+    request = Request(argv[1], data)
+    with urlopen(request) as webpage:
+        print(webpage.read().decode("utf-8"))
+
+
+if __name__ == "__main__":
+    hbtn_header()
