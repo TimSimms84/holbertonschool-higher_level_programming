558f11858e2602bec13f0a39f76651a9a511df5c
Tim <tsimms@gmail.com>
2022-03-08 13:02:32 -0600

    push 1, move 0 import

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,17 +4,15 @@ script that fetches https://intranet.hbtn.io/status
 """
 
 
-def hbtn_status():
+def hbtn_header():
     from urllib.request import Request, urlopen
+    from sys import argv
     """gets status of hbtn intranet"""
     request = Request("https://intranet.hbtn.io/status")
     with urlopen(request) as webpage:
-        webpage = webpage.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(webpage)))
-        print("\t- content: {}".format(webpage))
-        print("\t- utf8 content: {}".format(webpage.decode('utf8')))
+        header = webpage.info()
+        print(header['X-Request-Id'])
 
 
 if __name__ == "__main__":
-    hbtn_status()
+    hbtn_header()
