880ec79157769556dd64fc7476e301770eef3c20
Tim <tsimms@gmail.com>
2022-03-08 12:46:36 -0600

    first push

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
+
+
+
+
+
+def hbtn_status():
+    request = Request("https://intranet.hbtn.io/status")
+    with urlopen(request) as webpage:
+        webpage = webpage.read()
+        print("Body Response:")
+        print("\t- type: {}".format(type(webpage)))
+        print("\t- content: {}".format((webpage)))
+        print("\t- utf8 content: {}".format((webpage.decode('utf8'))))
+        
+
+
+
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    hbtn_status()
