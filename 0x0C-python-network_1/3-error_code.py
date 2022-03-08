0b72c39eae29de74e2b6ebf026af8f88d15fb86c
Tim <tsimms@gmail.com>
2022-03-08 13:32:49 -0600

    remove leftover code from 2

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -7,11 +7,16 @@ script that fetches https://intranet.hbtn.io/status
 def hbtn_header():
     from urllib.request import Request, urlopen
     from urllib.parse import urlencode
+    from urllib.error import HTTPError
     from sys import argv
     """gets status of hbtn intranet"""
     request = Request(argv[1])
-    with urlopen(request) as webpage:
-        print(webpage.read().decode("utf-8"))
+    try:        
+        with urlopen(request) as webpage:
+            webpage = webpage.read()
+            print(webpage.decode("utf-8"))
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
 
 
 if __name__ == "__main__":
