87f715e9c98b6c88ae98ff7d0149e2f8c96bb7fe
Tim <tsimms@gmail.com>
2022-03-08 17:25:51 -0600

    push 10

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -9,9 +9,9 @@ def gethib_creds():
     from requests import get
     from requests.auth import HTTPBasicAuth
     from sys import argv
-    requests = get('https://api.github.com/users/{}'.format(argv[1]),
-                   auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(requests.json().get('id'))
+    requests = get('https://api.github.com/user',
+                   auth=HTTPBasicAuth(argv[1], argv[2])).json()
+    print(requests.get('id'))
 
 
 if __name__ == "__main__":
