d6051135040617b775010ceb530b1e585e02c531
Tim <tsimms@gmail.com>
2022-03-08 13:47:22 -0600

    pep8

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -5,13 +5,13 @@ script that fetches https://intranet.hbtn.io/status
 
 
 def hbtn_header():
+    """"""
     from urllib.request import Request, urlopen
-    from urllib.parse import urlencode
     from urllib.error import HTTPError
     from sys import argv
     """gets status of hbtn intranet"""
     request = Request(argv[1])
-    try:     
+    try:
         with urlopen(request) as webpage:
             webpage = webpage.read()
             print(webpage.decode("utf-8"))
