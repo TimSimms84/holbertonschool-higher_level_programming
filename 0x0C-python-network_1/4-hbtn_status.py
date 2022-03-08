f14b328df382103b70bf96c4a933acf7ec836d12
Tim <tsimms@gmail.com>
2022-03-08 16:41:27 -0600

    add 4

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
+
+
+import re
+
+
+def hbtn_status():
+    """"""
+    from requests import get
+    request = get("https://intranet.hbtn.io/status")
+    text = request.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
+
+    
+
+if __name__ == "__main__":
+    hbtn_status()
