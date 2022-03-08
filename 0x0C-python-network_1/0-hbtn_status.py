ee06ac3c9e937e5c4fa03fd5fd503bc9dee114c9
Tim <tsimms@gmail.com>
2022-03-08 12:56:14 -0600

    fix print statements

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -8,10 +8,10 @@ def hbtn_status():
     request = Request("https://intranet.hbtn.io/status")
     with urlopen(request) as webpage:
         webpage = webpage.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(webpage)))
-        print("\t- content: {}".format((webpage)))
-        print("\t- utf8 content: {}".format((webpage.decode('utf8'))))
+        print("\t- content: {}".format(webpage))
+        print("\t- utf8 content: {}".format(webpage.decode('utf8'))
 
 
 if __name__ == '__main__':
