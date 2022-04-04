6c7dcc78b391345afb7ffe829a1b2428651b3084
Tim Simms <tsimms@gmail.com>
2022-04-04 14:38:20 -0500

    number not Number

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const argvContent = process.argv[2];
 if (isNaN(argvContent)) {
   console.log('Not a number');
 } else {
-  console.log('My Number:', argvContent);
+  console.log('My number:', argvContent);
 }
