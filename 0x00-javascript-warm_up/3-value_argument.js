0febd321075d48d1cd9dd319e98c4ae2b9c35741
Tim Simms <tsimms@gmail.com>
2022-04-04 13:33:01 -0500

    remove length

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const argvLength = process.argv.length;
 const argvContent = process.argv[2];
-if (argvLength === 2) {
+if (argvContent === undefined) {
   console.log('No argument');
-} else if (argvLength === 3) {
+} else {
   console.log(argvContent);
 }
