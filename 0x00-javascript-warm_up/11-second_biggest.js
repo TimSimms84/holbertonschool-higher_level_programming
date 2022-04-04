aa81d73bfeba4db1dbf44e4f78d2c25b67df1046
Tim Simms <tsimms@gmail.com>
2022-04-04 16:26:20 -0500

    add 11

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,8 @@
 
 const a = process.argv.slice(2);
 let maxNumber = 0;
-
-if (process.argv.length <= 3) {
-  console.log('0');
-} else {
+if (process.argv.length > 3) {
   a.sort();
-  maxNumber = a[a.length - 2];
-  console.log(maxNumber);
+  maxNumber = Number(a[a.length - 2]);
 }
+console.log(maxNumber);
