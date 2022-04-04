8b104c7863283b538166487f44e5a7c320e6c42a
Tim Simms <tsimms@gmail.com>
2022-04-04 15:59:13 -0500

    factorial of arg

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num < 0) {
+    return -1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+
+const a = process.argv[2];
+
+if (isNaN(a)) {
+  console.log(1);
+} else {
+  console.log(factorial(a));
+}
