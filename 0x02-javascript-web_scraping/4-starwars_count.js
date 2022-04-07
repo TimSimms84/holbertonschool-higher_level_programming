243b44a01a8364a3557e97aec02be8463acdbac8
Tim Simms <tsimms@gmail.com>
2022-04-07 13:21:22 -0500

    counts number of films wedge is in

---
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const website = process.argv[2];
+request(website, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const jsonReturn = JSON.parse(body).results;
+    let wedgeApperance = 0;
+    for (const movies in jsonReturn) {
+      const characters = jsonReturn[movies].characters;
+      for (const charIndex in characters) {
+        if (characters[charIndex].includes('18')) {
+          wedgeApperance += 1;
+        }
+      }
+    }
+    console.log(wedgeApperance);
+  }
+});
