f5b82283e1172615bc81e4486f9cf5ad7576d926
Tim Simms <tsimms@gmail.com>
2022-10-25 13:34:51 -0500

    advanced task

---
+++ 0x0B-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" doc string """
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from relationship_city import City
+    from relationship_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    new_state = State(name="Califronia", cities=[City(name="San Francisco")])
+    session.add(new_state)
+    session.commit()
+    session.close()
