62dc6e7cd37a272e81efab79e822f5cb4a8b36e2
Tim Simms <tsimms@gmail.com>
2022-10-25 13:38:54 -0500

    shebang

--- 0x0B-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0B-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    session = Session(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     new_state = State(name="Califronia", cities=[City(name="San Francisco")])
     session.add(new_state)
     session.commit()
