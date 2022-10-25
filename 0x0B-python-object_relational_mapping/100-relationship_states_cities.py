6d401e58c7de74c0b5018ab32fc91e2e30fa2b77
Tim Simms <tsimms@gmail.com>
2022-10-25 13:41:39 -0500

    I know how to spell, I promise

--- 0x0B-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0B-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     from relationship_state import Base, State
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
